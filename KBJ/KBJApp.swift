a7298616c9e1cd439ade31d666400ccd69ac0f03
Nick22morris <nick88morris22@gmail.com>
2021-06-11 13:18:46 -0500

    Initial Commit

---
+++ KBJ/KBJApp.swift
@@ -0,0 +1,17 @@
+//
+//  KBJApp.swift
+//  KBJ
+//
+//  Created by Other user on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct KBJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
