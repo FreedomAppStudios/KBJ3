61026775a0098897bde43c72b76189e9a14f0ee3
Nick22morris <nick88morris22@gmail.com>
2021-06-13 15:19:31 -0500

    added online support for deals

--- KBJ/Views/Screens/DealsView.swift
+++ KBJ/Views/Screens/DealsView.swift
@@ -10,18 +10,21 @@ import SwiftUI
 struct DealsView: View {
     @State private var angle: Double = 0
     @State private var zoom: Double = 0
+    @State var message = ""
+    @State var image = ""
     var body: some View {
         ZStack {
             Color(.systemRed)
                 .ignoresSafeArea(edges: .top)
             VStack {
                 Spacer()
-                Image("25%")
+                Image(image)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 250) //250
                     .clipShape(RoundedRectangle(cornerRadius: 25.0))
                     .onAppear(perform: {
+                        retrieveData()
                         if zoom < 1 {
                             angle += 360
                             zoom += 1
@@ -35,10 +38,11 @@ struct DealsView: View {
                     .animation(.easeIn)
 //                    .rotationEffect(.degrees(angle))
 //                    .animation(.interpolatingSpring(mass: 1, stiffness: 1, damping: 2, initialVelocity: 2))
-                Text("25% off your entire meal!")
-                    .font(.title)
+                Text(message)
+                    //.font(.title)
                     .bold()
                     .foregroundColor(.white)
+                    .font(.system(size: 50))
                 Text("Show this to your server at the end of the meal")
                     .foregroundColor(.white)
                     .font(.caption)
@@ -47,7 +51,33 @@ struct DealsView: View {
             }
         }
     }
-}
+    func retrieveData() {
+            db.collection("deals").getDocuments { querySnapshot, error in
+                if let e = error {
+                    message = "error fetching server"
+                    print(e)
+                } else {
+                    //staff = ["found documents"]
+                    if let snapshotDocuments = querySnapshot?.documents {
+                        //staff = ["got inside"]
+                        for doc in snapshotDocuments {
+                            //staff = ["even farther"]
+                            let data = doc.data()
+                            if let message1 = data["deal"] as? String, let image1 = data["image"] as? String{
+                                //let count = staff.count
+                               message = message1
+                                image = image1
+                            }
+                        }
+                    }
+                }
+            }
+            //    if staff == []{
+            //        staff = ["this error"]
+            //    }
+        }
+    }
+
 
 struct DealsView_Previews: PreviewProvider {
     static var previews: some View {
