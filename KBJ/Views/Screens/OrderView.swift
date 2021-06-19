f72d5d272f1a851cde354a8372cce6d056b057a5
nick22morris <nick88morris22@gmail.com>
2021-06-18 22:57:45 -0500

    added sum

--- KBJ/Views/Screens/OrderView.swift
+++ KBJ/Views/Screens/OrderView.swift
@@ -13,6 +13,7 @@ struct OrderView: View {
     @State var killed = [Int]()
     @State var hasRun = false
     @State var refreshed = false
+    @State var sum = 0.0
     var body: some View {
         NavigationView {
             if orderedItems.count == 0 {
@@ -34,40 +35,62 @@ struct OrderView: View {
                     
                 }
             } else {
-                List(orderedItems, id: \.foodName) { item in
-                    HStack {
-                        Text(item.foodName)
-                            .bold()
-                            .font(.title)
-                            .onAppear(perform: {
-                                        orderedItems.append(contentsOf: foodOrdered)
-                                foodOrdered = []
-                            })
-                        Spacer()
-                        Text(item.price)
-                            .bold()
-                            .font(.title)
-                        Button (action:{
-                            kill = item.place
-                            for num in killed {
-                                if kill > num {
-                                    kill -= 1
+                VStack {
+                    List(orderedItems, id: \.foodName) { item in
+                        HStack {
+                            Text(item.foodName)
+                                .bold()
+                                .font(.title)
+                                .onAppear(perform: {
+                                    orderedItems.append(contentsOf: foodOrdered)
+                                    foodOrdered = []
+                                })
+                            Spacer()
+                            Text(item.price)
+                                .bold()
+                                .font(.title)
+                                .onAppear(perform: {
+                                    var price = item.price
+                                    price.remove(at: price.startIndex)
+                                    sum += Double(price)!
+                                })
+                            Button (action:{
+                                var price = item.price
+                                price.remove(at: price.startIndex)
+                                sum -= Double(price)!
+                                
+                                kill = item.place
+                                for num in killed {
+                                    if kill > num {
+                                        kill -= 1
+                                    }
                                 }
+                                killed.append(kill)
+                                orderedItems.remove(at: kill)
+                            }) {
+                                Image(systemName: "minus.circle.fill")
                             }
-                            killed.append(kill)
-                            orderedItems.remove(at: kill)
-                        }) {
-                            Image(systemName: "minus.circle.fill")
                         }
                     }
+                    .navigationTitle("Order")
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 25)
+                            .frame(width: 250, height: 50)
+                            .foregroundColor(.red)
+                        Text("$" + String(sum))
+                            .font(.system(size: 45))
+                            .bold()
+                    }
+                    Spacer()
+                    Spacer()
+                    Spacer()
                 }
-                .navigationTitle("Order")
             }
-            
         }
         .onDisappear(perform: {
             hasRun = false
         })
+        
     }
 }
 
@@ -75,4 +98,18 @@ struct OrderView_Previews: PreviewProvider {
     static var previews: some View {
         OrderView()
     }
+}
+extension StringProtocol {
+    subscript(offset: Int) -> Character { self[index(startIndex, offsetBy: offset)] }
+    subscript(range: Range<Int>) -> SubSequence {
+        let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
+        return self[startIndex..<index(startIndex, offsetBy: range.count)]
+    }
+    subscript(range: ClosedRange<Int>) -> SubSequence {
+        let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
+        return self[startIndex..<index(startIndex, offsetBy: range.count)]
+    }
+    subscript(range: PartialRangeFrom<Int>) -> SubSequence { self[index(startIndex, offsetBy: range.lowerBound)...] }
+    subscript(range: PartialRangeThrough<Int>) -> SubSequence { self[...index(startIndex, offsetBy: range.upperBound)] }
+    subscript(range: PartialRangeUpTo<Int>) -> SubSequence { self[..<index(startIndex, offsetBy: range.upperBound)] }
 }
