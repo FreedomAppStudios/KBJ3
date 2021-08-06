4d7b30b9ea12924adbf3ac3ef8f44e9356d29f79
Nick22morris <nick88morris22@gmail.com>
2021-08-06 14:14:19 -0500

    updated specials for scroll

--- KBJ/Controllers/MenuView.swift
+++ KBJ/Controllers/MenuView.swift
@@ -14,146 +14,143 @@ struct MenuView: View {
     @State var type = ""
     @State var description = ""
     @State var price = ""
+    @State var specialArray = [FoodCell]()
     let db = Firestore.firestore()
     var body: some View {
         NavigationView {
             let frameHeight: CGFloat = 135.0
-            ScrollView(.vertical, showsIndicators: false) {
-                Text("")
-                    .onAppear() {
-                        db.collection("z-special").addSnapshotListener {
-                            querySnapshot, error in
-                            if let e = error {
-                                name = "error fetching server"
-                                print(e)
-                            } else {
-                                
-                                //staff = ["found documents"]
-                                if let snapshotDocuments = querySnapshot?.documents {
-                                    //staff = ["got inside"]
-                                    for doc in snapshotDocuments {
-                                        let data = doc.data()
-                                        if let n = data["name"] as? String,
-                                           let d = data["description"] as? String,
-                                           let i = data["image"] as? String,
-                                           let p = data["price"] as? String,
-                                           let t = data["type"] as? String {
-                                            urlFire = i
-                                            name = n
-                                            type = t
-                                            description = d
-                                            price = p
+            VStack {
+                ScrollView(.vertical, showsIndicators: false) {
+                    HStack {
+                        Text("Special")
+                            .font(.headline)
+                            .padding(.leading, 15)
+                            .padding(.top, 5)
+                        Spacer()
+                    }
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(alignment: .top, spacing: 0) {
+                            ForEach(specialArray, id: \.foodName) { food in
+                                NavigationLink(destination: FoodView(name: food.foodName, type: food.type, description: food.toppings, price: food.price, image: food.image, item: food)) {
+                                    SpecialItem(food: food)
+                                }
+                            }
+                        }
+                    }
+                    Text("")
+                        .onAppear() {
+                            db.collection("z-special").addSnapshotListener {
+                                querySnapshot, error in
+                                if let e = error {
+                                    name = "error fetching server"
+                                    print(e)
+                                } else {
+                                    specialArray = []
+                                    //staff = ["found documents"]
+                                    if let snapshotDocuments = querySnapshot?.documents {
+                                        //staff = ["got inside"]
+                                        for doc in snapshotDocuments {
+                                            let data = doc.data()
+                                            if let n = data["name"] as? String,
+                                               let d = data["description"] as? String,
+                                               let i = data["image"] as? String,
+                                               let p = data["price"] as? String,
+                                               let t = data["type"] as? String {
+                                                specialArray.append(FoodCell(foodName: n, toppings: d, image: i, price: p, type: t))
+                                                urlFire = i
+                                                name = n
+                                                type = t
+                                                description = d
+                                                price = p
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
+                    Divider()
+                    VStack(alignment: .leading) {
+                        Text("Fries")
+                            .font(.headline)
+                            .padding(.leading, 15)
+                            .padding(.top, 5)
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(alignment: .top, spacing: 0) {
+                                ForEach(foodFries, id: \.foodName) { food in
+                                    NavigationLink(destination: FoodView(name: food.foodName, type: food.type, description: food.toppings, price: food.price, image: food.image, item: food)) {
+                                        CategoryItem(food: food)
+                                    }
+                                }
+                            }
+                        }
+                        .frame(height: frameHeight)
+                        Text("Appetizers")
+                            .font(.headline)
+                            .padding(.leading, 15)
+                            .padding(.top, 5)
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(alignment: .top, spacing: 5) {
+                                ForEach(foodApps, id: \.foodName) { food in
+                                    NavigationLink(destination: FoodView(name: food.foodName, type: food.type, description: food.toppings, price: food.price, image: food.image, item: food)) {
+                                        CategoryItem(food: food)
+                                    }
+                                }
+                            }
+                        }
+                        .frame(height: frameHeight)
+                        
+                        Text("Burgers")
+                            .font(.headline)
+                            .padding(.leading, 15)
+                            .padding(.top, 5)
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(alignment: .top, spacing: 5) {
+                                ForEach(foodBurgers, id: \.foodName) { food in
+                                    NavigationLink(destination: FoodView(name: food.foodName, type: food.type, description: food.toppings, price: food.price, image: food.image, item: food)) {
+                                        CategoryItem(food: food)
+                                    }
+                                }
+                            }
+                        }
+                        .frame(height: frameHeight)
+                        
+                        Text("Sandwiches")
+                            .font(.headline)
+                            .padding(.leading, 15)
+                            .padding(.top, 5)
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(alignment: .top, spacing: 5) {
+                                ForEach(foodSandwiches, id: \.foodName) { food in
+                                    NavigationLink(destination: FoodView(name: food.foodName, type: food.type, description: food.toppings, price: food.price, image: food.image, item: FoodCell(foodName: food.foodName, toppings: food.toppings, image: food.image, price: food.price, type: food.type))) {
+                                        CategoryItem(food: food)
+                                    }
+                                }
+                            }
+                        }
+                        .frame(height: frameHeight)
+                        
+                        Text("Salads")
+                            .font(.headline)
+                            .padding(.leading, 15)
+                            .padding(.top, 5)
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(alignment: .top, spacing: 5) {
+                                ForEach(foodSalads, id: \.foodName) { food in
+                                    NavigationLink(destination: FoodView(name: food.foodName, type: food.type, description: food.toppings, price: food.price, image: food.image, item: FoodCell(foodName: food.foodName, toppings: food.toppings, image: food.image, price: food.price, type: food.type))) {
+                                        CategoryItem(food: food)
+                                    }
+                                }
+                            }
+                        }
+                        .frame(height: frameHeight)
+                        
                     }
-                NavigationLink(destination: FoodView(name: name, type: type, description: description, price: price, image: urlFire, item: FoodCell(foodName: name, toppings: description, image: urlFire, price: price, type: type))) {
-                    Image("wifi")
-                        .data(url: URL(string: urlFire)!)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 350, height: 200)
-                        .clipped()
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
                 }
-                HStack {
-                    Text("Special")
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
-                        .padding(.leading)
-                    Spacer()
-                }
-                HStack {
-                    Text(name)
-                        .font(.title)
-                        .padding(.leading)
-                    Spacer()
-                }
-                Divider()
-                VStack(alignment: .leading) {
-                    Text("Fries")
-                        .font(.headline)
-                        .padding(.leading, 15)
-                        .padding(.top, 5)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(alignment: .top, spacing: 0) {
-                            ForEach(foodFries, id: \.foodName) { food in
-                                NavigationLink(destination: FoodView(name: food.foodName, type: food.type, description: food.toppings, price: food.price, image: food.image, item: food)) {
-                                    CategoryItem(food: food)
-                                }
-                            }
-                        }
-                    }
-                    .frame(height: frameHeight)
-                    Text("Appetizers")
-                        .font(.headline)
-                        .padding(.leading, 15)
-                        .padding(.top, 5)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(alignment: .top, spacing: 5) {
-                            ForEach(foodApps, id: \.foodName) { food in
-                                NavigationLink(destination: FoodView(name: food.foodName, type: food.type, description: food.toppings, price: food.price, image: food.image, item: food)) {
-                                    CategoryItem(food: food)
-                                }
-                            }
-                        }
-                    }
-                    .frame(height: frameHeight)
-                    
-                    Text("Burgers")
-                        .font(.headline)
-                        .padding(.leading, 15)
-                        .padding(.top, 5)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(alignment: .top, spacing: 5) {
-                            ForEach(foodBurgers, id: \.foodName) { food in
-                                NavigationLink(destination: FoodView(name: food.foodName, type: food.type, description: food.toppings, price: food.price, image: food.image, item: food)) {
-                                    CategoryItem(food: food)
-                                }
-                            }
-                        }
-                    }
-                    .frame(height: frameHeight)
-                    
-                    Text("Sandwiches")
-                        .font(.headline)
-                        .padding(.leading, 15)
-                        .padding(.top, 5)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(alignment: .top, spacing: 5) {
-                            ForEach(foodSandwiches, id: \.foodName) { food in
-                                NavigationLink(destination: FoodView(name: food.foodName, type: food.type, description: food.toppings, price: food.price, image: food.image, item: FoodCell(foodName: food.foodName, toppings: food.toppings, image: food.image, price: food.price, type: food.type))) {
-                                    CategoryItem(food: food)
-                                }
-                            }
-                        }
-                    }
-                    .frame(height: frameHeight)
-                    
-                    Text("Salads")
-                        .font(.headline)
-                        .padding(.leading, 15)
-                        .padding(.top, 5)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(alignment: .top, spacing: 5) {
-                            ForEach(foodSalads, id: \.foodName) { food in
-                                NavigationLink(destination: FoodView(name: food.foodName, type: food.type, description: food.toppings, price: food.price, image: food.image, item: FoodCell(foodName: food.foodName, toppings: food.toppings, image: food.image, price: food.price, type: food.type))) {
-                                    CategoryItem(food: food)
-                                }
-                            }
-                        }
-                    }
-                    .frame(height: frameHeight)
-                    
-                }
-                
                 .navigationBarTitle("Menu")
             }
         }
--- KBJ/Models/CategoryView.swift
+++ KBJ/Models/CategoryView.swift
@@ -13,11 +13,13 @@ struct CategoryItem: View {
     var body: some View {
         VStack(alignment: .leading) {
             Image(food.image)
+                .data(url: URL(string: food.image)!)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 100, height: 100)
                 .cornerRadius(5)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
+            
             Text(food.foodName)
                 .font(.caption)
                 .foregroundColor(.primary)
@@ -26,6 +28,29 @@ struct CategoryItem: View {
         .padding(.leading, 15)
     }
 }
+
+struct SpecialItem: View {
+    var food: FoodCell
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(food.image)
+                .data(url: URL(string: food.image)!)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 350, height: 200)
+                .clipped()
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            
+            Text(food.foodName)
+                .font(.title)
+                .padding(.leading)
+                .foregroundColor(.primary)
+        }
+        .padding(.leading, 15)
+    }
+}
+
 
 struct CategoryItem_Previews: PreviewProvider {
     static var previews: some View {
